f8a5b8d0cd535999bf9f6b080f743043d2a3e86e
Leah Chan <lec533@g.harvard.edu>
2016-10-16 19:19:28 -0400

    testing routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel!!!=3
+                    Laravel
                 </div>
 
                 <div class="links">
